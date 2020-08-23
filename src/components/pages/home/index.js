f5cdaeaf9d69e780314becf299d835dc106853ba
framista <adajab1@gmail.com>
2020-08-23 19:41:20 +0200

    Create Introduction section and not found page file

---
+++ src/components/pages/home/index.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Introduction from '../introduction';
+
+const Home = () => {
+  return (
+    <Fragment>
+      <Introduction />
+    </Fragment>
+  );
+};
+
+export default Home;
---
+++ src/components/pages/introduction/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+const Introduction = () => {
+  return (
+    <div className="introduction">
+      <h2 className="introduction__name">Adrianna Jabłońska</h2>
+      <p className="introduction__job">Junior Frontend Developer</p>
+    </div>
+  );
+};
+
+export default Introduction;
