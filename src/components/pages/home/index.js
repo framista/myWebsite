81390eecfcc9753bafefd2c4727fbcdb6c4a8cff
framista <adajab1@gmail.com>
2020-08-24 12:51:45 +0200

    Change layout for introduction

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import Introduction from '../introduction';
+import Skill from '../skill';
 
 const Home = () => {
   return (
     <Fragment>
       <Introduction />
+      <Skill />
     </Fragment>
   );
 };
--- src/components/pages/introduction/index.js
+++ src/components/pages/introduction/index.js
@@ -6,6 +6,7 @@ const Introduction = () => {
     <div className="introduction">
       <h2 className="introduction__name">Adrianna Jabłońska</h2>
       <p className="introduction__job">Junior Frontend Developer</p>
+      <div className="triangle introduction__triangle"></div>
     </div>
   );
 };
