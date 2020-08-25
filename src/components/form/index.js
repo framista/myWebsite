3886aab580b6775a2e438587eed13d1d85de2eb1
framista <adajab1@gmail.com>
2020-08-25 14:06:20 +0200

    Create layout for contact form

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -5,9 +5,30 @@ const Form = () => {
   return (
     <div className="form">
       <form>
-        <h3>Let's stay in touch</h3>
-        <input type="text" />
-        <button type="submit">Send</button>
+        <h3 className="form__title">Let's stay in touch</h3>
+        <div className="form__group">
+          <label className="form__label">Your name</label>
+          <input className="form__input" type="text" name="name" />
+        </div>
+        <div className="form__group">
+          <label className="form__label">Your email</label>
+          <input className="form__input" type="email" name="email" />
+        </div>
+        <div className="form__group">
+          <label className="form__label">Subject</label>
+          <input className="form__input" type="text" name="subject" />
+        </div>
+        <div className="form__group">
+          <label className="form__label">Message</label>
+          <textarea
+            className="form__textarea"
+            rows="3"
+            name="message"
+          ></textarea>
+        </div>
+        <button className="form__button" type="submit">
+          Send
+        </button>
       </form>
     </div>
   );
