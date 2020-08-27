d24cce97587f94dfcdc821c6e9b74e7cf2fff756
framista <adajab1@gmail.com>
2020-08-27 13:53:41 +0200

    Create layout for not-found page

--- src/components/pages/not-found/index.js
+++ src/components/pages/not-found/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import './style.css';
+import { Link } from 'react-router-dom';
 
-const NotFoundPage = () => {
-  return <div>404</div>;
-};
+const NotFoundPage = () => (
+  <div className="not-found">
+    <div className="not-found__container">
+      <h1 className="not-found__title">Page not found</h1>
+      <p className="not-found__description">
+        I am very sorry for the inconvenience. It looks like you're trying to
+        access a page that has been deleted or never even existed.
+      </p>
+      <Link to="/" className="not-found__link">
+        Home
+      </Link>
+    </div>
+  </div>
+);
 
 export default NotFoundPage;
